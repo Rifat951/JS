ed6b3712df806d16d2323cd3458fa4746e062bdc
Rifat Hossain <31892050+Rifat951@users.noreply.github.com>
2022-01-09 04:06:40 +0600

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,32 @@
+
+//***********************//
+//         postfix      //
+
+console.log("Postfix....\n");
+let a = 2; // a =2
+a++; // a =2
+console.log(a++); // it will output 3
+// then additional 1 will be added in the next one
+console.log(a); //  a = 4
+
+
+//*******************//
+// Prefix //
+
+console.log("\nPrefix....");
+
+let b = 1;
+console.log(++b); // b = 2
+console.log(++b); // b = 3
+console.log(b++); // b = 3
+console.log(b); // b = 4
+
+
+// multiplication
+
+let x = 1 + 2 * (3*2);
+console.log ("Output " + x);
+
+
+// and && ..... or ||
+ 
