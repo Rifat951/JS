4ef9eb27d3b93cf3df646907dd5dc1c69305240b
Rifat Hossain <31892050+Rifat951@users.noreply.github.com>
2022-01-09 04:08:05 +0600

    Add files via upload

---
+++ Basics/natives.js
@@ -0,0 +1,12 @@
+
+/* let pattern = /xyz/;
+let val = 'This is xyz test';
+console.log(val.replace(pattern, 'just replace')); */
+
+let str = new String('hello'); // string over her works as a construtor
+//console.log(str);
+
+console.log(str.toString());
+console.log(str.toLowerCase());
+console.log(str.toUpperCase());
+console.log(str.length);
---
+++ Basics/obeject.js
@@ -0,0 +1,84 @@
+
+let car = {
+    make : 'benz',
+    modle : 'c-class',
+    year : 2016,
+    getprice : function(){
+        return 12000;
+    },
+    printdesc : function(){
+        console.log(this.make + ' '+this.modle+' '+this.year+' '+'has ' +this.getprice());
+    }
+    // under the first curly braces are object
+    // we put the object into the var
+};
+
+
+// . --> property access operator
+
+car.printdesc();
+console.log(car.year);
+
+// to access a property array element can be used
+console.log(car['year']);
+// this doesnt work
+console.log(car[2]);
+
+
+
+// another way to declare and print object 
+
+let testcar = {};
+testcar.carmake = 'toyota';
+console.log(testcar.carmake);
+
+
+// merge multiple objects together
+
+
+let a = {
+    property : {
+        b : {  
+            c : 'test test'
+        }
+    }
+};
+
+console.log(a.property.b.c);
+
+
+// array of objects
+// known as a graph of objects
+
+
+let carlot =
+    [
+        {year : 2016, make : 'benz', model : 's-class'},
+        {year : 2016, make : 'bmw', model : '320i'},
+        {year : 2016, make : 'toyota', model : 'crown'},
+    ]
+
+ // it can be iterated and print
+
+for(let i = 0 ; i < carlot.length; i++){
+    console.log(carlot[i]);
+}
+
+// system that can contain the data of employees
+
+let contact = {
+    employees : [
+        {firstname : 'test', contact: ['0001', '0002']}
+    ],
+    customers : [
+        {firstname : 'dontknow', contact : ['0003', '0005']}
+    ]
+};
+
+// it can be iterated using loop
+//console.log(contact.customers[0]);
+
+
+for (let i = 0; i < contact.customers.length; i++){
+    console.log(contact.customers[i]);
+}
